2ced91f502c9b7645b10425ea0a094c46192597a
LAB13_USER ON WS32 <L13Y12W32@LabHCI.net>
2026-02-07 12:40:58 +0800

    Backend setup, added inital entity, controller, repository, and service files

---
+++ backend/miniapp/src/main/java/com/dalapo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.dalapo.repository;
+
+
+import com.dalapo.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    
+    UserEntity findByEmail(String email);
+    
+}
